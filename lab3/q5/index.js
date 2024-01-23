3a91960c578b8349873e325d0e0201f05794b7dc
student <swapnil.strange0707@gmail.com>
2024-01-23 11:15:31 +0530

    Added the feature to get and remove table on request

--- lab3/q5/index.js
+++ lab3/q5/index.js
@@ -1,1 +1,31 @@
-const table=
+const table = document.getElementsByTagName('table')[0]
+const button = document.querySelector('.btn')
+let tableExists = false;
+
+const updateTable = () => {
+    for (let i = 0; i <= 10; i++) {
+        table.innerHTML += `<tr>
+        <td>${i}</td>
+        <td>${i * i}</td>
+        <td>${i * i * i}</td>
+    </tr>`
+    }
+    button.textContent = "Remove Table"
+}
+
+button.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (!tableExists) {
+        updateTable()
+        tableExists = true
+    }
+    else {
+        tableExists=false
+        table.innerHTML = `<tr>
+        <th>Number</th>
+        <th>Square</th>
+        <th>Cube</th>
+    </tr>`
+        button.textContent = "Get Table"
+    }
+})
